06b52b3700833cbc3e35cf38eabfae4e9e6a193b
amiti-17 <timjobit@gmail.com>
2023-09-12 01:23:25 +0300

    move nest to 8080 port

--- nestapp/src/main.ts
+++ nestapp/src/main.ts
@@ -4,7 +4,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
+  await app.listen(8080);
 }
 
 bootstrap();
