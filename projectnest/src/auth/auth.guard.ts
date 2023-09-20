a59d49e9ca64e8456f3ac5f0e8da7b6823fe9f61
amiti-17 <timjobit@gmail.com>
2023-09-20 10:01:47 +0300

    auth guard now has @SkipAuth to skip auth)

--- projectnest/src/auth/auth.guard.ts
+++ projectnest/src/auth/auth.guard.ts
@@ -7,14 +7,26 @@ import {
 import { JwtService } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
 import { Request } from 'express';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from './auth.module';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest();
-    console.log('request: ', request);
     const token = this.extractTokenFromHeader(request);
     if (!token) throw new UnauthorizedException();
     try {
--- projectnest/src/auth/auth.module.ts
+++ projectnest/src/auth/auth.module.ts
@@ -4,6 +4,9 @@ import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
+import { AuthGuard } from './auth.guard';
+import { APP_GUARD } from '@nestjs/core';
+import { SetMetadata } from '@nestjs/common';
 
 @Module({
   imports: [
@@ -14,8 +17,17 @@ import { jwtConstants } from './constants';
       signOptions: { expiresIn: '300s' },
     }),
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   controllers: [AuthController],
   exports: [AuthService],
 })
 export class AuthModule {}
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const SkipAuth = () => SetMetadata(IS_PUBLIC_KEY, true);
